ad8e14685bbd0aae47a3cbd131958394511599e9
SachinDhakarey <sachindhakarey2002@gmail.com>
2023-04-06 02:02:56 +0530

    using JS in our program

---
+++ script.js
@@ -0,0 +1,5 @@
+// alert("Hello")
+
+setTimeout(function(){
+    alert(" Get Breaking News Alerts From NDTV We'll send you latest news updates through the day. You can manage them any time from your browser settings  ")
+},3000)
