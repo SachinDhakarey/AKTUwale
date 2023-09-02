0470a24b11b21d46bd148819e6a8baca26fff9f6
Ipankajrawat <pr326573@gmail.com>
2023-09-02 13:24:20 +0530

    Removed Redundant Comment

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// alert("Hello")
-
 setTimeout(function(){
     alert(" Get Breaking News Alerts From NDTV We'll send you latest news updates through the day. You can manage them any time from your browser settings  ")
 },3000)
